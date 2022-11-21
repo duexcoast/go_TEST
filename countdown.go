df0225316feda2418e15c216d56b2d77e28fd34d
conor.ney <conor.ux@gmail.com>
2022-11-21 11:01:54 -0500

    used dependency injection to mock countdown time.Sleep()

--- countdown.go
+++ countdown.go
@@ -4,12 +4,39 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
-func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+type DefaultSleeper struct {}
+
+func(d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func(s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
