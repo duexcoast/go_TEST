63b99b8614127690c1f992e2f32762d061d2448e
conor.ney <conor.ux@gmail.com>
2022-11-21 10:27:20 -0500

    added countdown test and switch package to main

--- di.go
+++ di.go
@@ -1,20 +1,10 @@
-package maps
+package main
 
 import (
 	"fmt"
 	"io"
-	"log"
-	"net/http"
 )
 
 func Greet(writer io.Writer, name string) {
     fmt.Fprintf(writer, "Hello, %s", name)
-}
-
-func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
-    Greet(w, "world")
-}
-
-func main() {
-    log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
 }
