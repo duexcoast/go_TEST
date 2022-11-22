51dcce41b2a4a1ec75197b1610ff3632bde04ee2
conor.ney <conor.ux@gmail.com>
2022-11-22 15:35:58 -0500

    test for channels

--- walk_test.go
+++ walk_test.go
@@ -94,6 +94,26 @@ func TestWalk(t *testing.T) {
 		assertContains(t, got, "Chris")
 		assertContains(t, got, "London")
 	})
+	t.Run("with channels", func(t *testing.T) {
+		aChannel := make(chan Profile)
+
+		go func() {
+			aChannel <- Profile{33, "Berlin"}
+			aChannel <- Profile{11, "Osaka"}
+			close(aChannel)
+		}()
+
+		var got []string
+		want := []string{"Berlin", "Osaka"}
+
+		Walk(aChannel, func(input string){
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }
 
 func assertContains(t testing.TB, haystack []string, needle string) {
