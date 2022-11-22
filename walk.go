bff0c15604168bd72b27e5a8d4564d2770eef140
conor.ney <conor.ux@gmail.com>
2022-11-22 13:55:48 -0500

    Walk() succesfully deals with pointers

--- walk.go
+++ walk.go
@@ -7,6 +7,13 @@ func Walk(x interface{}, fn func(input string)) {
 	// reflect.ValueOf returns us the value of a given variable
 	val	:= reflect.ValueOf(x)
 
+	// you can use NumField on a pointer value, so we have to use 
+	// an if statement outside of the loop
+	if val.Kind() == reflect.Pointer {
+		// Elem() returns the value than an interface contains or a pointer points to
+		val = val.Elem()
+	}
+
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
 		
