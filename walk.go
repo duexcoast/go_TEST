355afe4b00967ca078558dd9a2ada74cc8fed97a
conor.ney <conor.ux@gmail.com>
2022-11-22 13:59:55 -0500

    extracted getValue into its own function

--- walk.go
+++ walk.go
@@ -4,19 +4,12 @@ import "reflect"
 
 // x interface{} is the same as type any
 func Walk(x interface{}, fn func(input string)) {
-	// reflect.ValueOf returns us the value of a given variable
-	val	:= reflect.ValueOf(x)
 
-	// you can use NumField on a pointer value, so we have to use 
-	// an if statement outside of the loop
-	if val.Kind() == reflect.Pointer {
-		// Elem() returns the value than an interface contains or a pointer points to
-		val = val.Elem()
-	}
+	val := getValue(x)
 
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
-		
+
 		switch field.Kind() {
 		case reflect.String:
 			fn(field.String())
@@ -24,4 +17,16 @@ func Walk(x interface{}, fn func(input string)) {
 			Walk(field.Interface(), fn)
 		}
 	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	// you can use NumField on a pointer value, so we have to use
+	// an if statement outside of the loop
+	if val.Kind() == reflect.Pointer {
+		// Elem() returns the value than an interface contains or a pointer points to
+		val = val.Elem()
+	}
+	return val
 }
