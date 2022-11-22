fcefe2a10ddc1f186f9d6cc951a8d95775c920af
conor.ney <conor.ux@gmail.com>
2022-11-22 00:49:13 -0500

    refactored to use concurrency

--- racer.go
+++ racer.go
@@ -2,21 +2,32 @@ package main
 
 import (
 	"net/http"
-	"time"
+
 )
 
 func Racer(a, b string) (winner string) {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
+
+	// select lets you wait on multiple channels. The first one to send a value "wins"
 	
-	if aDuration < bDuration {
+	select {
+	case <-ping(a):
 		return a
+	case <-ping(b):
+		return b
 	}
-	return b
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ping(url string) chan struct{} {
+	// chan struct{} is the smallest data type available from a memory perspective
+	// we get no allocation versus a bool. we're not sending anything, so why allocate?
+	// always make channels versus assigning to a var. var ch chan struct{} initializes with a zero value,
+	// which would be nil in this case. 
+	// If we try to send to a nil channel with <-, it will block forever bc you can't send to nil channels. 
+	ch := make(chan struct{})
+	go func() {
+		// sends signal into channel once we have compelted http.Get
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
