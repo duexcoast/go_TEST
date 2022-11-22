71ce5bdabc4e9afa6747e1e5bf414763395ff1dc
conor.ney <conor.ux@gmail.com>
2022-11-22 18:24:31 -0500

    minimal amount of passing code to pass cancel/ctx test

--- server_test.go
+++ server_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 type StubStore struct {
@@ -16,14 +18,36 @@ func (s *StubStore) Fetch() string {
 
 func TestServer(t *testing.T) {
 	data := "hello world"
-	svr := Server(&StubStore{data})
 
-	request := httptest.NewRequest(http.MethodGet, "/", nil)
-	response := httptest.NewRecorder()
+	t.Run("returns data from store", func(t *testing.T) {
+		store := &SpyStore{response: data}
+		svr := Server(store)
 
-	svr.ServeHTTP(response, request)
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+		svr.ServeHTTP(response, request)
 
-	if response.Body.String() != data {
-		t.Errorf("want %s got %s", data, response.Body.String())
-	}
+		if response.Body.String() != data {
+			t.Errorf("want %s got %s", data, response.Body.String())
+		}
+	})
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		data := "hello world"
+		store := &SpyStore{response: data}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+
+		response := httptest.NewRecorder()
+		
+		svr.ServeHTTP(response, request)
+
+		if !store.cancelled {
+			t.Error("store was not told to cancel")
+		}
+	})
 }
