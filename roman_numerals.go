01958c427580eac491cbcbc895de35a63070fb3e
conor.ney <conor.ux@gmail.com>
2022-11-24 13:04:37 -0500

    added tons of test conditions, and extended AllRomanNumerals dataset to go up to 1000"

--- roman_numerals.go
+++ roman_numerals.go
@@ -3,11 +3,25 @@ package main
 import "strings"
 
 type RomanNumeral struct {
-	Value int
+	Value  int
 	Symbol string
 }
 
-var AllRomanNumerals = []RomanNumeral {
+// declaring rules about roman numerals as data,
+// rather than hiding them inside a switch / case
+// inside of the algorithm.
+// switch statements should normally be a red flag that we might
+// be capturing a concept or data inside imperative code, when
+// it might be better captured in a class structure instead.
+var AllRomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
 	{10, "X"},
 	{9, "IX"},
 	{5, "V"},
@@ -15,7 +29,7 @@ var AllRomanNumerals = []RomanNumeral {
 	{1, "I"},
 }
 
-func ConvertToRoman (arabic int) string {
+func ConvertToRoman(arabic int) string {
 
 	var result strings.Builder
 
